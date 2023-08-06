b9428e7afa5ef3e6de3a1fea088b57aa66dd957f
Irsyaad Budi <irsyaad@privyid.tech>
2023-08-06 11:02:18 +0700

    feat: add table

--- components/PaginationEditor.vue
+++ components/PaginationEditor.vue
@@ -15,11 +15,21 @@ import Subscript from "@tiptap/extension-subscript";
 import Superscript from "@tiptap/extension-superscript";
 import Highlight from "@tiptap/extension-highlight";
 import Image from "@tiptap/extension-image";
-import { Node } from "@tiptap/core";
-import { PAGE_BREAK_FORMAT, PAPER_HEIGHT, SPARATOR_HEIGHT } from "~/constants";
-import { PAGE_BREAK_FORMAT_CHECK } from "~/constants";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
 
-const test = ref();
+import { Node } from "@tiptap/core";
+import {
+  PAGE_BREAK_FORMAT,
+  PAGE_BREAK_FORMAT_CHECK,
+  PAPER_HEIGHT,
+  SPARATOR_HEIGHT,
+} from "~/constants";
+
+const test = ref("");
+const isOnpaste = ref(false);
 const PageBreak = Node.create({
   name: "pageBreak",
   addOptions() {
@@ -46,11 +56,391 @@ const PageBreak = Node.create({
     return ["div", mergeAttributes(HTMLAttributes)];
   },
 });
+const CustomTableCell = TableCell.extend({
+  addAttributes() {
+    return {
+      // extend the existing attributes …
+      ...this.parent?.(),
+
+      borderBottom: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderBottom))
+            return {};
+          console.log("asdad");
+          return {
+            style: `border-bottom: ${attributes.borderBottom}`,
+          };
+        },
+      },
+      borderLeft: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderLeft)) return {};
+          return {
+            style: `border-left: ${attributes.borderLeft}`,
+          };
+        },
+      },
+      borderRight: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderRight)) return {};
+          return {
+            style: `border-right: ${attributes.borderRight}`,
+          };
+        },
+      },
+      borderTop: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderTop)) return {};
+          return {
+            style: `border-top: ${attributes.borderTop}`,
+          };
+        },
+      },
+      border: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.border)) return {};
+          return {
+            style: `border: ${attributes.border}`,
+          };
+        },
+      },
+      verticalAlign: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.verticalAlign))
+            return {};
+          return {
+            style: `vertical-align: ${attributes.verticalAlign}`,
+          };
+        },
+      },
+      padding: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.padding)) return {};
+          return {
+            style: `padding: ${attributes.padding}`,
+          };
+        },
+      },
+      paddingTop: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.paddingTop)) return {};
+          return {
+            style: `padding-top: ${attributes.paddingTop}`,
+          };
+        },
+      },
+      paddingLeft: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.paddingLeft)) return {};
+          return {
+            style: `padding-left: ${attributes.paddingLeft}`,
+          };
+        },
+      },
+      paddingBottom: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.paddingBottom))
+            return {};
+          return {
+            style: `padding-bottom: ${attributes.paddingBottom}`,
+          };
+        },
+      },
+      overflow: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.overflow)) return {};
+          return {
+            style: `overflow: ${attributes.overflow}`,
+          };
+        },
+      },
+      overflowWrap: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.overflowWrap))
+            return {};
+          return {
+            style: `overflow-wrap: ${attributes.overflowWrap}`,
+          };
+        },
+      },
+    };
+  },
+});
+const CustomTableRow = TableRow.extend({
+  addAttributes() {
+    return {
+      // extend the existing attributes …
+      ...this.parent?.(),
+
+      borderBottom: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderBottom))
+            return {};
+          return {
+            style: `border-bottom: ${attributes.borderBottom}`,
+          };
+        },
+      },
+      borderLeft: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderLeft)) return {};
+          return {
+            style: `border-left: ${attributes.borderLeft}`,
+          };
+        },
+      },
+      borderRight: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderRight)) return {};
+          return {
+            style: `border-right: ${attributes.borderRight}`,
+          };
+        },
+      },
+      borderTop: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderTop)) return {};
+          return {
+            style: `border-top: ${attributes.borderTop}`,
+          };
+        },
+      },
+      border: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.border)) return {};
+          return {
+            style: `border: ${attributes.border}`,
+          };
+        },
+      },
+      verticalAlign: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.verticalAlign))
+            return {};
+          return {
+            style: `vertical-align: ${attributes.verticalAlign}`,
+          };
+        },
+      },
+      padding: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.padding)) return {};
+          return {
+            style: `padding: ${attributes.padding}`,
+          };
+        },
+      },
+      paddingTop: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.paddingTop)) return {};
+          return {
+            style: `padding-top: ${attributes.paddingTop}`,
+          };
+        },
+      },
+      paddingLeft: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.paddingLeft)) return {};
+          return {
+            style: `padding-left: ${attributes.paddingLeft}`,
+          };
+        },
+      },
+      paddingBottom: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.paddingBottom))
+            return {};
+          return {
+            style: `padding-bottom: ${attributes.paddingBottom}`,
+          };
+        },
+      },
+      overflow: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.overflow)) return {};
+          return {
+            style: `overflow: ${attributes.overflow}`,
+          };
+        },
+      },
+      overflowWrap: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.overflowWrap))
+            return {};
+          return {
+            style: `overflow-wrap: ${attributes.overflowWrap}`,
+          };
+        },
+      },
+    };
+  },
+});
+const CustomTableHeader = TableHeader.extend({
+  addAttributes() {
+    return {
+      // extend the existing attributes …
+      ...this.parent?.(),
+
+      // and add a new one …
+      // backgroundColor: {
+      //   default: null,
+      //   // parseHTML: (element) => element.getAttribute("data-background-color"),
+      //   renderHTML: (attributes) => {
+      //     return {
+      //       "data-background-color": attributes.backgroundColor,
+      //       style: `background-color: ${attributes.backgroundColor}`,
+      //     };
+      //   },
+      // },
+      // border-bottom: solid #000000 0.5pt;
+      // vertical-align: bottom;
+      // padding: 0pt 5.4pt 0pt 5.4pt;
+      // overflow: hidden;
+      // overflow-wrap: break-word;
+      borderBottom: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderBottom))
+            return {};
+          return {
+            style: `border-bottom: ${attributes.borderBottom}`,
+          };
+        },
+      },
+      borderLeft: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderLeft)) return {};
+          return {
+            style: `border-left: ${attributes.borderLeft}`,
+          };
+        },
+      },
+      borderRight: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderRight)) return {};
+          return {
+            style: `border-right: ${attributes.borderRight}`,
+          };
+        },
+      },
+      borderTop: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.borderTop)) return {};
+          return {
+            style: `border-top: ${attributes.borderTop}`,
+          };
+        },
+      },
+      border: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.border)) return {};
+          return {
+            style: `border: ${attributes.border}`,
+          };
+        },
+      },
+      verticalAlign: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.verticalAlign))
+            return {};
+          return {
+            style: `vertical-align: ${attributes.verticalAlign}`,
+          };
+        },
+      },
+      padding: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.padding)) return {};
+          return {
+            style: `padding: ${attributes.padding}`,
+          };
+        },
+      },
+      paddingTop: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.paddingTop)) return {};
+          return {
+            style: `padding-top: ${attributes.paddingTop}`,
+          };
+        },
+      },
+      paddingLeft: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.paddingLeft)) return {};
+          return {
+            style: `padding-left: ${attributes.paddingLeft}`,
+          };
+        },
+      },
+      paddingBottom: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.paddingBottom))
+            return {};
+          return {
+            style: `padding-bottom: ${attributes.paddingBottom}`,
+          };
+        },
+      },
+      overflow: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.overflow)) return {};
+          return {
+            style: `overflow: ${attributes.overflow}`,
+          };
+        },
+      },
+      overflowWrap: {
+        default: "",
+        renderHTML: (attributes) => {
+          if ([undefined, null, ""].includes(attributes.overflowWrap))
+            return {};
+          return {
+            style: `overflow-wrap: ${attributes.overflowWrap}`,
+          };
+        },
+      },
+    };
+  },
+});
 
 const props = defineProps({
   modelValue: {
     type: String,
-    default: undefined,
+    default: "",
   },
   uploadImageFn: {
     type: Function,
@@ -140,15 +530,17 @@ const editor = useEditor({
       inline: true,
     }),
     PageBreak,
+    Table.configure({
+      resizable: true,
+    }),
+    CustomTableRow,
+    CustomTableHeader,
+    // TableCell,
+    CustomTableCell,
   ],
   onUpdate(props) {
     // HTML
-    // vModel.value = value.editor.getHTML();
-
-    // let newValue = value.editor.getHTML();
-
     vModel.value = props.editor.getHTML();
-    // console.log(props.editor.getHTML(), "update");
   },
   onSelectionUpdate(event) {
     if (event.editor?.isActive("heading")) {
@@ -171,6 +563,7 @@ const editor = useEditor({
     attributes: {
       class:
         "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl focus:outline-none",
+      style: "any",
     },
   },
 });
@@ -193,6 +586,7 @@ defineExpose({
 watch(
   vModel,
   (newValue, oldValue) => {
+    if (isOnpaste.value) return;
     const editorContainer = document.querySelector(
       ".page--placeholder"
     ) as HTMLDivElement;
@@ -205,7 +599,7 @@ watch(
       if (shouldAddPageBreak(currentPageCount, editorContainer)) {
         newValue = (newValue || "").replace(/<p><\/p>(?![\s\S]*<p><\/p>)/, "");
         newValue += PAGE_BREAK_FORMAT;
-
+        console.log("add page break");
         editor.value?.commands.setContent(newValue || "", true);
         return;
       }
@@ -216,9 +610,8 @@ watch(
           currentPageCount
         )
       ) {
-        const indexEmpty = separatePage.findIndex(
-          (item) => item === "" || item === "<p></p>"
-        );
+        console.log("delete page break");
+        const indexEmpty = separatePage.findIndex((item) => item === "");
 
         if (indexEmpty < 1) return;
 
@@ -258,17 +651,7 @@ const shouldAddPageBreak = (
 };
 
 function shouldDeletePageBreak(content: string, page: number) {
-  //   console.log(page > 1, isPageEmpty(content));
-  return page > 1 && (content === "" || content === "<p></p>");
-}
-
-function isPageEmpty(content: string) {
-  const condition = ["", "<p></p>"];
-
-  return !!condition.find((cond) => {
-    console.log({ cond, content, res: cond === content });
-    return cond === content;
-  });
+  return page > 1 && content === "";
 }
 
 function handlePaste(event: any) {
@@ -278,21 +661,73 @@ function handlePaste(event: any) {
 
   // Process the pasted text or apply custom logic
   // const transformedText = transformPastedText(pastedText);
-  const transformedText = pastedText;
-  test.value = pastedText;
-  // editor.value?.commands.setContent(transformedText);
-  // editor.value?.commands.insertContent(transformedText);
+  // const transformedText = pastedText;
+  // test.value = pastedText;
+  // editor.value?.commands.setContent(transformedText, true);
+  const docxHtml = `
+    <table style="border: none; border-collapse: collapse">
+      <colgroup>
+        <col width="734" />
+      </colgroup>
+      <tbody>
+        <tr style="height: 0pt">
+          <td
+            style="
+              border-bottom: solid #000000 0.5pt;
+              vertical-align: bottom;
+              padding: 0pt 5.4pt 0pt 5.4pt;
+              overflow: hidden;
+              overflow-wrap: break-word;
+            "
+          >
+            <br />
+          </td>
+        </tr>
+        <tr style="height: 0pt">
+          <td
+            style="
+              border-bottom: solid #000000 0.5pt;
+              border-top: solid #000000 0.5pt;
+              vertical-align: bottom;
+              padding: 0pt 5.4pt 0pt 5.4pt;
+              overflow: hidden;
+              overflow-wrap: break-word;
+            "
+          >
+            <br />
+          </td>
+        </tr>
+        <tr style="height: 0pt">
+          <td
+            style="
+              border-bottom: solid #000000 0.5pt;
+              border-top: solid #000000 0.5pt;
+              vertical-align: bottom;
+              padding: 0pt 5.4pt 0pt 5.4pt;
+              overflow: hidden;
+              overflow-wrap: break-word;
+            "
+          >
+            <br />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  `;
+
+  editor.value?.commands.insertContent(docxHtml);
 }
 
-function transformPastedText(text: string) {
-  // Apply your custom transformations to the pasted text
-  return text.toUpperCase(); // Example: converting text to uppercase
-}
+// function transformPastedText(text: string) {
+//   // Apply your custom transformations to the pasted text
+//   return text.toUpperCase(); // Example: converting text to uppercase
+// }
 </script>
 
 <style lang="scss">
 .page--placeholder {
   @apply bg-white;
+  // @apply px-6 py-8;
   //   width: 794px;
   //   height: 1123px;
 }
